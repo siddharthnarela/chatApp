ca2b68cc901422bea88851421fc91b31c6434bd7
Siddharth Narela <74176439+siddharthnarela@users.noreply.github.com>
2024-04-09 11:55:26 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCoHtIi4kkwDCKtOxntNIBvBUMVYIhp7rE",
-  authDomain: "chat-110ca.firebaseapp.com",
-  projectId: "chat-110ca",
-  storageBucket: "chat-110ca.appspot.com",
-  messagingSenderId: "909335024365",
-  appId: "1:909335024365:web:c8e9e584fd7e26da5db2e4",
-  measurementId: "G-D7HPPWBREQ"
+  apiKey :, 
+  authDomain: ,
+  projectId:,
+  storageBucket: ,
+  messagingSenderId: ",
+  appId: "1:909335024365:web:,
+  measurementId: "
 };
 
 const app = initializeApp(firebaseConfig);
